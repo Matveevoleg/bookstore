5f64839590407ef4055d6777e29663a45d40264d
Matveevoleg <johndoe@example.com>
2017-03-15 12:38:34 +0200

    added omiauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,6 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "322754198120390", "e602f0060992911aef98d9eb9b4a0d85"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
 end
