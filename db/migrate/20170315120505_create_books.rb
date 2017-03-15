035b3645e13fc9923e22e179175094f66376d6ad
Matveevoleg <johndoe@example.com>
2017-03-15 15:10:17 +0200

    added book, author, category

---
+++ db/migrate/20170315120505_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :books do |t|
+      t.string 		:title,	null: false		
+      t.text		:descriptions,	default: ''
+      t.decimal		:price, precision: 8, scale: 2, null: false
+      t.integer		:quantity, default: 1
+      t.integer		:year, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315094719) do
+ActiveRecord::Schema.define(version: 20170315130004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "first_name", null: false
+    t.string   "last_name",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title",                                             null: false
+    t.text     "descriptions",                         default: ""
+    t.decimal  "price",        precision: 8, scale: 2,              null: false
+    t.integer  "quantity",                             default: 1
+    t.integer  "year",                                              null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+  end
+
+  create_table "books_authors", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "book_id"
+    t.index ["author_id"], name: "index_books_authors_on_author_id", using: :btree
+    t.index ["book_id"], name: "index_books_authors_on_book_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -37,4 +67,6 @@ ActiveRecord::Schema.define(version: 20170315094719) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "books_authors", "authors"
+  add_foreign_key "books_authors", "books"
 end
